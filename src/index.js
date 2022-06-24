b715283bde523d4dfdcc484c65334d4ac5ed707f
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-06-24 18:54:02 +0600

    User and admin api created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-const express = require('express');
-const env = require('dotenv');
-const app = express()
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+
+// Import routes
+const userRoutes = require("./routes/user");
 
 // Environment Variable
 env.config();
 
-app.get('/', (req, res) =>{
-    res.status(200).json({
-        message: 'Hello form server'
-    })
-})
+// MongoDB Connection
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@flipkartclone.ffshs.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("Database Connected");
+  });
 
+// Middlewares
+app.use(bodyParser.json());
+app.use("/api", userRoutes);
+
+// Listen the PORT
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on PORT ${process.env.PORT}`);
-})
+  console.log(`Server is running on PORT ${process.env.PORT}`);
+});
